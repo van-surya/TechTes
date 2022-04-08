6bd1b042382d595bf7222c03915d51f8c31c5ee2
surevan08 <suryavan08@gmail.com>
2022-04-08 14:31:02 +0700

    Jawaban

--- Bonuscase/bonus_case.sql
+++ Bonuscase/bonus_case.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 08, 2022 at 09:18 AM
+-- Generation Time: Apr 08, 2022 at 09:29 AM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 7.4.15
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `bonuscase`
+-- Database: `bonus_case`
 --
 
 -- --------------------------------------------------------
